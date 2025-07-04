5a1b873b7b7bf4c2e2b510ca6a27f819c0b57551
student <student@FK-0832>
2025-07-04 16:54:01 +0900

    新規開発プロジェクト

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO categories(name) VALUES('食費');
+INSERT INTO categories(name) VALUES('家賃');
+INSERT INTO categories(name) VALUES('交通費');
+INSERT INTO categories(name) VALUES('雑費');
+INSERT INTO categories(name) VALUES('交際費');
+INSERT INTO categories(name) VALUES('ガソリン代');
+
+INSERT INTO records(user_id,date,category_id,title,detail,price)values('1','2025-08-04','1','昼食','カレー','1000');
+
+INSERT INTO accounts(name,password) VALUES('越川詩織','shiori0123');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+drop table if exists accounts;
+drop table if exists records;
+drop table if exists categories;
+
+create table accounts(
+id serial primary key,
+	name text,
+	password text	
+);
+
+
+create table records(
+	id serial primary key,
+	user_id int,
+	date date,
+	category_id int,
+	title text,
+	detail text,
+	price int
+);
+
+create table categories(
+	id serial primary key,
+	name text	
+);
+
